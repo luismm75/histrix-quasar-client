69f9401ce4281bc784322b754e54962e8db5c38d
Luis Melgratti <luis@mundoit.com.ar>
2020-11-20 02:44:03 -0300

    q-editor improve

--- ui/src/components/HistrixField.vue
+++ ui/src/components/HistrixField.vue
@@ -3,6 +3,7 @@
     <span class="q-pa-xs text-caption" v-if="isRadio" >
       {{  label }}
     </span>
+
     <component
       v-bind:is="fieldComponent"
       v-model="localValue"
@@ -17,9 +18,19 @@
       :inner="true"
       :mask="fieldMask"
       :filled="!isDisabled"
-      
+      :toolbar="toolbar"      
       :disabled="isDisabled"
       :readonly="isDisabled"
+      :fonts="{
+        arial: 'Arial',
+        arial_black: 'Arial Black',
+        comic_sans: 'Comic Sans MS',
+        courier_new: 'Courier New',
+        impact: 'Impact',
+        lucida_grande: 'Lucida Grande',
+        times_new_roman: 'Times New Roman',
+        verdana: 'Verdana'
+      }"
       
       :size="size"
       :_readonly="schema.readonly == 'readonly'"
@@ -33,6 +44,8 @@
       inline
       dense
     >
+
+
       <!-- in control button -->
       <template v-slot:append>
         <!-- EXTERNAL HELP POPUP -->
@@ -278,6 +291,7 @@ export default {
       helpFoundes: true,
       options: [],
       rules: [],
+      toolbar: [],
       type: 'q-input',
     };
   },
@@ -367,6 +381,87 @@ export default {
           break;
         case 'q-editor':
           component = 'q-editor';
+          this.toolbar = [
+            [
+              {
+                label: this.$q.lang.editor.align,
+                icon: this.$q.iconSet.editor.align,
+                fixedLabel: true,
+                list: 'only-icons',
+                options: ['left', 'center', 'right', 'justify']
+              },
+            ],
+            ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+            ['token', 'hr', 'link', 'custom_btn'],
+            ['print', 'fullscreen'],
+            [
+              {
+                label: this.$q.lang.editor.formatting,
+                icon: this.$q.iconSet.editor.formatting,
+                list: 'no-icons',
+                options: [
+                  'p',
+                  'h1',
+                  'h2',
+                  'h3',
+                  'h4',
+                  'h5',
+                  'h6',
+                  'code'
+                ]
+              },
+              {
+                label: this.$q.lang.editor.fontSize,
+                icon: this.$q.iconSet.editor.fontSize,
+                fixedLabel: true,
+                fixedIcon: true,
+                list: 'no-icons',
+                options: [
+                  'size-1',
+                  'size-2',
+                  'size-3',
+                  'size-4',
+                  'size-5',
+                  'size-6',
+                  'size-7'
+                ]
+              },
+              {
+                label: this.$q.lang.editor.defaultFont,
+                icon: this.$q.iconSet.editor.font,
+                fixedIcon: true,
+                list: 'no-icons',
+                options: [
+                  'default_font',
+                  'arial',
+                  'arial_black',
+                  'comic_sans',
+                  'courier_new',
+                  'impact',
+                  'lucida_grande',
+                  'times_new_roman',
+                  'verdana'
+                ]
+              },
+              'removeFormat'
+            ],
+            ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+
+            ['undo', 'redo'],
+            ['viewsource']
+          ];
+          /*
+          :fonts="{
+            arial: 'Arial',
+            arial_black: 'Arial Black',
+            comic_sans: 'Comic Sans MS',
+            courier_new: 'Courier New',
+            impact: 'Impact',
+            lucida_grande: 'Lucida Grande',
+            times_new_roman: 'Times New Roman',
+            verdana: 'Verdana'
+          }"
+          */
           break;
         case 'q-input':
           component = 'q-input';
