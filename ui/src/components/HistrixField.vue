204445d2e899a25ee6fcd2ca90a9aaa073612271
Pablo Bagliere <pablo@mundoit.com.ar>
2025-04-21 10:11:30 -0300

    Actualiza versión de quasar-ui-histrix-client a 0.0.170 y mejora la validación en HistrixField

--- ui/src/components/HistrixField.vue
+++ ui/src/components/HistrixField.vue
@@ -1150,6 +1150,7 @@ export default {
             return this.value;
           }
           if (this.value === '') return false;
+          if (typeof this.value === 'string') return this.value !== '0';
           return this.value !== '0';
         }
         if (this.isDate) {
